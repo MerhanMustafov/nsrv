acc3810ced73acad9651d0bea2bcc260f89a39a0
unknown <m.o.mustafov@gmail.com>
2022-08-07 18:02:14 +0300

    add

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT || 5151,
     // DB_CONNECTION_STRING: `mongodb://localhost:27017/Notes`,
     DB_CONNECTION_STRING: `mongodb+srv://npn:5151-npp1515-0w@cluster0.8ijirbc.mongodb.net/test`,
     TOKEN_SECRET: 'this is secure!',
