38b2a7616d627c2979f9deacdd68c95d88c927c5
unknown <m.o.mustafov@gmail.com>
2022-08-07 22:13:05 +0300

    add changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { PORT, DB_CONNECTION_STRING } = require('./config')
-// const db = require('./config/dbConfig')
 const cors = require('./config/cors')
 const userController = require('./controllers/userController')
 const listController = require('./controllers/listController')
@@ -14,18 +13,14 @@ async function serverOn() {
       useUnifiedTopology: true,
     })
     const db = mongoose.connection
-    console.log(`DB connected ${DB_CONNECTION_STRING}`)
-    // db.on('error', (err) => console.log(err.message))
-    // db.once('open', () => console.log(`DB connected ${DB_CONNECTION_STRING}`))
+    console.log(`DB connected !`)
   } catch (err) {
     console.log(err.message)
   }
   const app = express()
-//   await db(app)
-//   app.use(express.json())
-  app.use(express.json({limit: '50mb'}));
-    app.use(express.urlencoded({extended: true, limit: '50mb'}));
- 
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+
   app.use(cors())
 
   app.use('/user', userController)
@@ -33,18 +28,10 @@ async function serverOn() {
   app.use('/note', noteCotroller)
   app.get('*', (req, res) => res.status(404).json('Not found !'))
 
-
   app.listen(PORT, () => {
-    console.log(`Server is ON port: ${PORT}`)
+    console.log(`Server is ON !`)
   })
 }
 
-
-//  app.use(express.json({ limit: '50mb' }))
-//   app.use(
-//     express.urlencoded({
-//       limit: '50mb',
-//       extended: true,
-//       parameterLimit: 50000,
-//     }),
-//   )
+// db.on('error', (err) => console.log(err.message))
+// db.once('open', () => console.log(`DB connected ${DB_CONNECTION_STRING}`))
