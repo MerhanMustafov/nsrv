db1c01b641bd967d103a7be5246a571385343262
unknown <m.o.mustafov@gmail.com>
2022-08-12 15:47:19 +0300

    add

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    PORT: process.env.BASEURL || 5151,
+    PORT: process.env.PORT || 5151,
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     TOKEN_SECRET: process.env.TOKEN_SECRET
     // DB_CONNECTION_STRING: "mongodb://localhost:27017/Notes",
