44182ab3c6160a80e0d4e556cd856d0b76afe6fb
unknown <m.o.mustafov@gmail.com>
2022-08-19 14:54:35 +0300

    add

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', `${process.env.ORIGIN}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH,DELETE, OPTIONS, HEAD');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
     next()
