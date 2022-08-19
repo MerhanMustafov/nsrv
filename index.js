262b80cd6b49b9db503968c2ba7b0ad1e55bb75c
unknown <m.o.mustafov@gmail.com>
2022-08-19 18:04:23 +0300

    add

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const commentCotroller = require('./controllers/commentController')
 serverOn()
 
 async function serverOn() {
-    console.log(process.env.ORIGIN)
   try {
     mongoose.connect(DB_CONNECTION_STRING, {
       useNewUrlParser: true,
